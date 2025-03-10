1a873bfd9bb784bbc03bf588a1de54d08086ed67
bharadwaj21122001 <bharadwaj.21122001@gmail.com>
2025-03-10 17:39:29 +0530

    Added components to display the content.

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -11,15 +11,40 @@ const ContextProvider = (props) => {
     const [loading,setLoading] = useState(false);
     const [resultData,setResultData] = useState("");
 
-    const onSent = async (prompt) => {
+    const delayPara = (index,nextWord) => {
+        setTimeout(function () {
+            setResultData(prev=>prev+nextWord);
+        },75*index)
+    }
+    const onSent = async () => {
+        if(!input.trim()) return;
 
         setResultData(""); // Clear previous result
         setLoading(true); // Set loading to true before API call
         setShowResult(true);
+        setRecentPrompt(input)
+        setPrevPrompts(prev=>[...prev,input])
 
         try{
             const response = await run(input); // Pass 'prompt' to the API
-            setResultData(response); // Store response for display
+            let responseArray = response.split("**");
+            let newResponse;
+            for (let i = 0; i < responseArray.length; i++)
+            {
+                if (i === 0 || i%2 !== 1) {
+                    newResponse += responseArray[i];
+                }
+                else {
+                    newResponse += "<b>"+responseArray[i]+"</b>";
+                }
+            }
+            let newResponse2 = newResponse.split("*").join("</br>");
+            let newResponseArray = newResponse2.split(" ");
+            for (let i=0; i<newResponseArray.length; i++)
+            {
+                const nextWord = newResponseArray[i];
+                delayPara(i,nextWord+" ");
+            }
         } catch (error) {
             console.error("Error in onSent:", error);
             setResultData("Failed to fetch response.");
